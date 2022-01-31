4c4a97c226d035a74d14d88901e24ad33c704344
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-01-31 08:55:17 +0100

    es6-inicioBotones

---
+++ 3eso/2022/2t/calculadora/js/app.js
@@ -0,0 +1,19 @@
+function inicio(){
+    console.log ('cargando ...');
+
+    const display = document.querySelector("span#display");
+    const uno = document.querySelector("span#b1");
+    const dos = document.querySelector("span#b2");
+    const mas = document.querySelector("span#bmas");
+    console.log(uno, display);
+    uno.addEventListener("click",() => {
+        console.log("uno");
+        display.innerText = display.innerText.concat("1");
+    });
+    dos.addEventListener("click",() => {
+        console.log("dos");
+        display.innerText = display.innerText.concat("2");
+    });
+
+
+}
