f04c232a02b2aacdb1d934e0bc3e336df32b03a4
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-02-06 23:32:43 +0100

    operacion + con memoria

--- 3eso/2022/2t/calculadora/js/app.js
+++ 3eso/2022/2t/calculadora/js/app.js
@@ -1,21 +1,57 @@
 function inicio(){
     console.log ('cargando ...');
+    let numero1;
+    let memoria = 0;
+    let operacion;
+    let resultado;
 
     const display = document.querySelector("span#display");
+    const cero = document.querySelector("span#b0");
     const uno = document.querySelector("span#b1");
     const dos = document.querySelector("span#b2");
+    const tres = document.querySelector("span#b3");
+    const cuatro = document.querySelector("span#b4");
+    const cinco = document.querySelector("span#b5");
+    const seis = document.querySelector("span#b6");
+    const siete = document.querySelector("span#b7");
+    const ocho = document.querySelector("span#b8");
+    const nueve = document.querySelector("span#b9");
+    
     const mas = document.querySelector("span#bmas");
-    console.log(uno, display);
+    const igual = document.querySelector("span#bigual");
+    
     uno.addEventListener("click",() => {
-        console.log("uno");
         display.innerText = display.innerText.concat("1");
-        console.log (result());
+        display.innerText = parseInt(display.innerText, 10);
     });
     dos.addEventListener("click",() => {
-        console.log("dos");
         display.innerText = display.innerText.concat("2");
+        display.innerText = parseInt(display.innerText, 10);
     });
-
+    tres.addEventListener("click",() => {
+        display.innerText = display.innerText.concat("3");
+        display.innerText = parseInt(display.innerText, 10);
+    });
+    cuatro.addEventListener("click",() => {
+        display.innerText = display.innerText.concat("4");
+        display.innerText = parseInt(display.innerText, 10);
+    });
+    cinco.addEventListener("click",() => {
+        display.innerText = display.innerText.concat("5");
+        display.innerText = parseInt(display.innerText, 10);
+    });
+    bmas.addEventListener("click",() => {
+        memoria = parseInt(display.innerText, 10);
+        display.innerText = "+";
+        operacion = '+';
+    });
+    bigual.addEventListener("click",() => {
+        if (operacion == '+') {
+            resultado = parseInt(display.innerText, 10) + memoria;
+            display.innerText = resultado;
+        }
+        
+    });
 
 }
 
